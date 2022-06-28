05a699ded9bb7543cc930b2eaf8c8ca019dccb59
Daryl Rogony <darylrogony@gmail.com>
2022-06-28 12:25:58 +0300

    Insertion of Components

---
+++ src/components/Footer.tsx
@@ -0,0 +1,19 @@
+// import Github from './Github';
+
+function Footer() {
+  return (
+    <footer>
+      {/* <Github username="kiprogony" /> */}
+      <a
+        href="https://rawg.io/apidocs"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="Button"
+      >
+        TAKE A LOOK AT RAWG API
+      </a>
+    </footer>
+  );
+}
+
+export default Footer;
